5892e415811d6cdbd5f868d815f1bc5c563cd03a
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-10-26 08:11:10 +0530

    c

---
+++ parttren/2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, row = 0;
+    // printf("enter the values\n");
+    // scanf("%d", &row);
+    for (a = 1; a <= 5; a++)
+    {
+        for (b = 1; b <= a; b++)
+            printf("%d", a);
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ parttren/3.c
@@ -0,0 +1,22 @@
+
+
+// 6. program for pattern
+
+#include <stdio.h>
+int main()
+{
+    int a, b, row = 1, sum = 1;
+    printf("enter the values\n");
+    scanf(" %d", &row);
+    for (a = 1; a <= row; a++)
+    {
+        for (b = 1; b <= a; b++)
+        {
+            printf(" %d", sum);
+            sum++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
