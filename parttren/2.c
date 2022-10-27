e56160e40dec845b7ed43fa04f4d7f9b84ced4dd
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-10-27 14:30:54 +0530

    cc

--- parttren/2.c
+++ parttren/2.c
@@ -2,6 +2,9 @@
 int main()
 {
     int a, b, row = 0;
+    char c;
+    float d;
+    double e;
     // printf("enter the values\n");
     // scanf("%d", &row);
     for (a = 1; a <= 5; a++)
@@ -10,6 +13,10 @@ int main()
             printf("%d", a);
         printf("\n");
     }
-
+    printf(".....%d....", sizeof(a));
+    printf("%d", sizeof(b));
+    printf("%d", sizeof(c));
+    printf("%d", sizeof(d));
+    printf("%d", sizeof(e));
     return 0;
 }
